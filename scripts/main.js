7bf6ed166fdcfddbc9489e16d73f3b408974fb1f
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-02-20 15:06:57 +0000

    laser update but not hitting

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,6 @@ $(() => {
   const $grid = $('.grid')
   const board = []
 
-
-  // player tracking
-  let playerCurrentIndex = 275
-
-
   for(let i = 0; i < 285 ; i++) {
     const $gridbox = document.createElement('div')
     $gridbox.className = 'gridbox'
@@ -18,13 +13,13 @@ $(() => {
   }
 
   const $gridbox = $('.gridbox')
+  const lasers = []
 
 
   // ALIENS
 
   class Alien {
     constructor(startingIndex) {
-      // this.startingIndex = startingIndex
       this.currentIndex = startingIndex
       this.currentMoves = 0
       this.isMovingRight = true
@@ -32,6 +27,7 @@ $(() => {
       this.render()
       this.movementId = null
       this.move()
+
     }
 
     render() {
@@ -40,41 +36,59 @@ $(() => {
 
     move() {
       this.movementId = setInterval(() => {
-        $(board).eq(this.currentIndex).removeClass('alien')
-        if (this.currentMoves < 6) {
-          this.currentMoves++
-          if(this.isMovingRight) {
-            this.currentIndex++
+        if (this.currentIndex < 266) {
+          $(board).eq(this.currentIndex).removeClass('alien')
+          if (this.currentMoves < 6) {
+            this.currentMoves++
+            if(this.isMovingRight) {
+              this.currentIndex++
+            } else {
+              this.currentIndex--
+            }
           } else {
-            this.currentIndex--
+            this.currentIndex += 19
+            this.currentMoves = 0
+            this.isMovingRight = !this.isMovingRight
           }
+          this.render()
         } else {
-          this.currentIndex += 19
-          this.currentMoves = 0
-          this.isMovingRight = !this.isMovingRight
+          alert('Game over!')
+          // this.gamePlaying = false
+          clearInterval(this.movementId)
         }
-        this.render()
+
       }, 1000)
     }
   }
 
-
   // Place aliens and call object Alien
   const aliens = [new Alien(20), new Alien(22), new Alien(24), new Alien(26), new Alien(28), new Alien(30), new Alien(40), new Alien(42), new Alien(44), new Alien(46), new Alien(48), new Alien(58), new Alien(60), new Alien(62), new Alien(64), new Alien(66), new Alien(68)]
 
-  console.log(aliens)
 
 
-
+  // PLAY BUTTON
   // when play button is clicked - create formation:
   const $playButton = $('.playButton')
 
   $playButton.on('click', (e) => {
-    // call Setup function
+    console.log('playing')
+    this.gameplaying = true
+
+  })
+  const $quitButton = $('.quitButton')
+
+  $quitButton.on('click', (e) => {
+    console.log('quit')
+    this.gameplaying = false
+    clearInterval(this.movementId)
+    // does this need to refer to the alien?
+    $(window).on('keydown')
   })
 
 
   // PLAYER
+  let playerCurrentIndex = 275
+
   const $player = $(board[playerCurrentIndex]).addClass('player')
 
   $(window).on('keydown', (e) => {
@@ -91,9 +105,10 @@ $(() => {
         break
       case 32:
         console.log('fire')
-        new Laser(playerCurrentIndex).fire()
+        const newLaser = new Laser(playerCurrentIndex).fire()
+        lasers.push(newLaser)
 
-        console.log(playerCurrentIndex, '<------- playerCurrentIndex')
+        // console.log(playerCurrentIndex, '<------- playerCurrentIndex')
         break
       default:
         console.log('hit any other key')
@@ -111,7 +126,7 @@ $(() => {
     constructor(firingIndex) {
       this.startingFire = firingIndex
       this.currentFire = firingIndex
-      console.log('fire', this.currentFire)
+      // console.log('fire', this.currentFire)
       this.currentMoves = 0
     }
 
@@ -119,32 +134,36 @@ $(() => {
       const laserFire = setInterval(() => {
         $(board).eq(this.currentFire).removeClass('laser')
         if (this.currentFire >= 19) {
-          // if ($gridbox.class !== 'alien') {
-          this.currentFire  -= 19
-          $(board).eq(this.currentFire).addClass('laser')
-          // this.currentIndex = playerCurrentIndex
+          this.currentFire -= 19
+          console.log(this.currentFire)
+          // if ($(this.currentFire).hasClass('alien')) {
+          //   console.log('HIT')
           // } else {
-          //   // detect collision
-          //   Alien.isHit = true
-          //   clearInterval(laserFire)
+          $(board).eq(this.currentFire).addClass('laser')
           // }
         } else {
           clearInterval(laserFire)
         }
-        console.log(this.currentFire, '<-------- this.currentFire')
-      }, 250)
+        // console.log(this.currentFire, '<-------- this.currentFire')
+      }, 450)
     }
+
   }
-  //
 
 
-//  can only have one laser on the screen at once
 
+  // function checkHit(laserElement) {
+  //   const laserIndex = lasers.currentFire[laserElement]
+  //   for (let x = 0; x < aliens.position.length; x++) {
+  //     if (aliens.position[x] === laserIndex) {
+  //       aliens.position.splice(x, 1)
+  //       lasers.position.splice(laserElement, 1)
+  //       $(board[laserIndex]).classList.remove('alien', 'laser')
+  //       console.log('hit')
+  //     }
+  //   }
+  // }
 
-// when fired(space), rember the class and every 2.5 second, remover laser class and add one to index + 19
-// at every loop, check whether the div has a class of alien
-// if yes, change this.isHit of alien, and remove colour. and then end laser
-// if no, continue to next box
 
 
 
